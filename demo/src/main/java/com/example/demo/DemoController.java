198d091dfee8ba18b70ea2638f3d96d694b1de42
Yu Mun Pan <munpanyu.govtech@gmail.com>
2022-03-21 18:39:38 +0800

    base commit

---
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class DemoController {
+    private List<String> startingMessages = List.of(
+            "Logic will get you from A to B. Imagination will take you everywhere.",
+            "There are 10 kinds of people. Those who know binary and those who don't.",
+            "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+            "It's not that I'm so smart, it's just that I stay with problems longer.",
+            "It is pitch dark. You are likely to be eaten by a grue."
+    );
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", startingMessages.get(new Random().nextInt(4 - 0 + 1) + 0));
+        model.addAttribute("placeholderImage", "yesser");
+
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "")
+                    String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "index"; //view
+    }
+}
